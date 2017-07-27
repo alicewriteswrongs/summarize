1afc65a09471c2282ac1d9def1ed0f313bfd1803
Alice Pote <alice.writes.wrongs@gmail.com>
2017-07-27 11:15:16 -0400

    fmt

--- summarize.js
+++ summarize.js
@@ -1,41 +1,44 @@
 const GithubAPI = require("github")
 
-const github = new GithubAPI();
+const github = new GithubAPI()
 
 const getOwnerAndRepo = repoName => ({
   owner: repoName.match(/^([a-z0-9-_]+)\//)[1],
-  repo: repoName.match(/\/([a-z0-9-_]+)$/)[1],
+  repo: repoName.match(/\/([a-z0-9-_]+)$/)[1]
 })
 
-const fetchInfo = async () => {
-  let activity = await github
-    .activity
-    .getEventsForUser({ username: 'aliceriot' })
+const fetchInfo = async username => {
+  let activity = await github.activity.getEventsForUser({
+    username
+  })
 
-  let pushesToMaster = activity.data.filter(event => (
-    event.type === 'PushEvent' &&
-    event.payload.ref === 'refs/heads/master'
-  ))
+  let pushesToMaster = activity.data.filter(
+    event =>
+      event.type === "PushEvent" && event.payload.ref === "refs/heads/master"
+  )
 
-  let commits = await Promise.all(pushesToMaster.map(async event => {
-    const { owner, repo } = getOwnerAndRepo(event.repo.name);
+  let commits = await Promise.all(
+    pushesToMaster.map(async event => {
+      const { owner, repo } = getOwnerAndRepo(event.repo.name)
 
-    const commit = await github.gitdata.getCommit({ 
-      sha: event.payload.head,
-      repo,
-      owner,
-    });
+      const commit = await github.gitdata.getCommit({
+        sha: event.payload.head,
+        repo,
+        owner
+      })
 
-    return {
-      message: commit.data.message,
-      repo: repo
-    }
-  }));
+      return {
+        message: commit.data.message,
+        repo: repo
+      }
+    })
+  )
 
-  console.log('Yesterday I merged the following:\n');
+  console.log("Yesterday I merged the following:\n")
+
   commits.forEach(({ message, repo }) => {
-    console.log(`- ${message.split('\n')[0]} (${repo})`);
-  });
-};
+    console.log(`- ${message.split("\n")[0]} (${repo})`)
+  })
+}
 
-fetchInfo();
+fetchInfo('aliceriot')
