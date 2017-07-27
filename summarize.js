e89ce15a5c0816062c0eb8e0bf9730e951c33a27
Alice Pote <alice.writes.wrongs@gmail.com>
2017-07-27 11:10:29 -0400

    it works!

--- summarize.js
+++ summarize.js
@@ -1,3 +1,41 @@
-const github = require('github')
+const GithubAPI = require("github")
 
+const github = new GithubAPI();
 
+const getOwnerAndRepo = repoName => ({
+  owner: repoName.match(/^([a-z0-9-_]+)\//)[1],
+  repo: repoName.match(/\/([a-z0-9-_]+)$/)[1],
+})
+
+const fetchInfo = async () => {
+  let activity = await github
+    .activity
+    .getEventsForUser({ username: 'aliceriot' })
+
+  let pushesToMaster = activity.data.filter(event => (
+    event.type === 'PushEvent' &&
+    event.payload.ref === 'refs/heads/master'
+  ))
+
+  let commits = await Promise.all(pushesToMaster.map(async event => {
+    const { owner, repo } = getOwnerAndRepo(event.repo.name);
+
+    const commit = await github.gitdata.getCommit({ 
+      sha: event.payload.head,
+      repo,
+      owner,
+    });
+
+    return {
+      message: commit.data.message,
+      repo: repo
+    }
+  }));
+
+  console.log('Yesterday I merged the following:\n');
+  commits.forEach(({ message, repo }) => {
+    console.log(`- ${message.split('\n')[0]} (${repo})`);
+  });
+};
+
+fetchInfo();
